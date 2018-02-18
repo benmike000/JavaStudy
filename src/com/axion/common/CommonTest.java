d364a45c9e38bab7d3c3aa18f9ebc7efa4b2cccb
zgyao <benmike1982@163.com>
2018-02-18 08:45:55 +0800

    A common test

---
+++ src/com/axion/common/CommonTest.java
@@ -0,0 +1,10 @@
+package com.axion.common;
+
+public class CommonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
